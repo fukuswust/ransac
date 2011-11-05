7e5ade140cc6dafb15aee3d25a5de862f92a294a
tdickman <tdickman@gmail.com>
2011-11-05 04:23:00 +0000

    Created initial depthToZ script - played around with possible optimizations.

--- MATLAB_Scripts/mainScript.m
+++ MATLAB_Scripts/mainScript.m
@@ -2,6 +2,9 @@
 inRawData = csvread([dataPath '/depthData/000015.csv']);
 
 %% Interchange 0's for NaN
+tic
 depthData = replaceValue(inRawData,2047,NaN);
+toc
 
 %% Convert Depth to Z
+convertedDepthData = depthToZ(depthData);
