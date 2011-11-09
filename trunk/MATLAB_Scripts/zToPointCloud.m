03561bb1b164ccc85e07813410b35f11ed1ffbdf
gideonjn@gmail.com <gideonjn@gmail.com@122d840c-6846-e998-9d05-f63fbce1aa5e>
2011-11-09 21:30:23 +0000

    Input initial equation

--- trunk/MATLAB_Scripts/zToPointCloud.m
+++ trunk/MATLAB_Scripts/zToPointCloud.m
@@ -1,5 +1,5 @@
 function [ pointCloud ] = zToPointCloud( zArray )
-%ZTOPOINTCLOUD Summary of this function goes here
+%ZTOPOINTCLOUD Convert array of depths to point cloud
 %   Input
 %       zArray - Array with z coordinates with resolution of 1mm
 %   Output
@@ -10,16 +10,23 @@ function [ pointCloud ] = zToPointCloud( zArray )
 %
 %   Change Log
 %       11/02/2011 - John Gideon - Created Shell Script
-
+%       11/09/2011 - John Gideon - Initial equation input and tested
+    
+    fx_d = 5.9421434211923247e+02;
+	fy_d = 5.9104053696870778e+02;
+	cx_d = 3.3930780975300314e+02;
+	cy_d = 2.4273913761751615e+02;
+    
     pointCloud(:,:,1) = zeros(size(zArray,1),size(zArray,2));
     pointCloud(:,:,2) = zeros(size(zArray,1),size(zArray,2));
     pointCloud(:,:,3) = zArray;
     for i = 1:size(zArray,1)
         for j = 1:size(zArray,2)
             %Equation goes here
-            pointCloud(i,j,1) = i;
-            pointCloud(i,j,2) = j;
-            pointCloud(i,j,3) = zArray(i,j);
+            z = zArray(i,j);
+            pointCloud(i,j,1) = ((j - cx_d) * z) / fx_d;
+            pointCloud(i,j,2) = ((i - cy_d) * z) / fy_d;
+            pointCloud(i,j,3) = z;
         end
     end
 
